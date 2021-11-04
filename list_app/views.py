09e7f1a7f9c8da1025be4de2abcddd30281e46a3
Abhijeet Manny <abhijeetmanny@gmail.com>
2021-11-04 01:32:35 +0100

    Add frontend with required views and urls

--- list_app/views.py
+++ list_app/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    print(search)
+    output_for_search ={
+        'search': search,
+    }
+    return render(request, 'list_app/new_search.html', output_for_search)
